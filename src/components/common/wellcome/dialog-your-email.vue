b4802e94658f848a60c889d0db374af3e8a1cad6
Duc Nguyen <ducnv@bachasoft.com>
2022-12-08 21:37:18 +0700

    sdsds

--- src/components/common/wellcome/dialog-your-email.vue
+++ src/components/common/wellcome/dialog-your-email.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="w-full h-full user-profile absolute top-0 p-8">
     <BtBack></BtBack>
-    <h2 class="mb-4 text-xl font-bold text-white">What's your email?</h2>
+    <h2 class="mb-2 mt-10 text-xl font-bold text-white">What's your email?</h2>
 
     <div class="text-color">
       Don't lose access to your account, verify your email
     </div>
+
+    <div class="mt-3">
+      <el-input
+        class="your-email mb-3 p-2"
+        v-model="txtFirstName"
+        placeholder="Your name"
+        @keyup="onChangeInput"
+      />
+    </div>
+
+    <BtContinue></BtContinue>
+    <div>OR</div>
+    <el-button
+      class="cursor-pointer bg-white text-base text-black w-80 rounded-lg px-6 py-6 mb-4"
+      @click="onLoginGoogle()"
+    >
+      <img src="@/assets/images/google_icon.svg" class="mr-2" alt="" />
+      <span class="font-sans">Log in with Google</span>
+    </el-button>
+
+    <div>Verify instantly by connecting your Google account</div>
+
+    <el-radio :label="3"
+      >I want to receive news, updates and offers from Heartlink</el-radio
+    >
   </div>
 </template>
 
 <script>
+import BtContinue from "../button/bt-continue";
 import BtBack from "../button/bt-back";
 export default {
   components: {
+    BtContinue,
     BtBack,
   },
   name: "dialog-your-email",
@@ -35,5 +62,11 @@ export default {
 }
 .user-profile {
   background-color: #232937;
+}
+
+.el-input__wrapper {
+  background-color: #495063;
+  padding: 10px;
+  border: 1px solid #495063;
 }
 </style>
